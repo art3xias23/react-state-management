11e6629dc06b124886f2ea7f448e69bc18df7155
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-21 21:55:49 +0000

    Replaced promise with async await

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import {getProducts} from "./services/productService"
+import { getProducts } from "./services/productService";
 import Spinner from "./Spinner";
 
 const products = [
   {
-    "id": 1,
-    "category": "shoes",
-    "image": "shoe1.jpg",
-    "name": "Hiker",
-    "price": 94.95,
-    "skus": [
-      { "sku": "17", "size": 7 },
-      { "sku": "18", "size": 8 }
+    id: 1,
+    category: "shoes",
+    image: "shoe1.jpg",
+    name: "Hiker",
+    price: 94.95,
+    skus: [
+      { sku: "17", size: 7 },
+      { sku: "18", size: 8 },
     ],
-    "description": "This rugged boot will get you up the mountain safely."
+    description: "This rugged boot will get you up the mountain safely.",
   },
   {
-    "id": 2,
-    "category": "shoes",
-    "image": "shoe2.jpg",
-    "name": "Climber",
-    "price": 78.99,
-    "skus": [
-      { "sku": "28", "size": 8 },
-      { "sku": "29", "size": 9 }
+    id: 2,
+    category: "shoes",
+    image: "shoe2.jpg",
+    name: "Climber",
+    price: 78.99,
+    skus: [
+      { sku: "28", size: 8 },
+      { sku: "29", size: 9 },
     ],
-    "description": "Sure-footed traction in slippery conditions."
+    description: "Sure-footed traction in slippery conditions.",
   },
   {
-    "id": 3,
-    "category": "shoes",
-    "image": "shoe3.jpg",
-    "name": "Explorer",
-    "price": 145.95,
-    "skus": [
-      { "sku": "37", "size": 7 },
-      { "sku": "38", "size": 8 },
-      { "sku": "39", "size": 9 }
+    id: 3,
+    category: "shoes",
+    image: "shoe3.jpg",
+    name: "Explorer",
+    price: 145.95,
+    skus: [
+      { sku: "37", size: 7 },
+      { sku: "38", size: 8 },
+      { sku: "39", size: 9 },
     ],
-    "description": "Look stylish while stomping in the mud."
-  }
-]
+    description: "Look stylish while stomping in the mud.",
+  },
+];
 
 export default function App() {
   const [size, setSize] = useState("");
@@ -51,9 +51,21 @@ export default function App() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() =>{
-    getProducts("shoes").then((response) => setProducts(response)).catch((e) => setError(e)).finally(() => setLoading(false));
-  },[])
+  useEffect(() => {
+    async function init() {
+      try {
+        const response = await getProducts("shoes");
+        setProducts(response);
+      } catch (error) {
+        setError(error);
+      }
+      finally{
+        setLoading(false);
+      }
+    }
+
+    init();
+  }, []);
 
   function renderProduct(p) {
     return (
@@ -66,9 +78,11 @@ export default function App() {
       </div>
     );
   }
-const filteredProducts = size ? products.filter((x) => x.skus.find((y) => y.size == size)) : products;
-if(error) throw error;
-if(loading) return <Spinner></Spinner>;
+  const filteredProducts = size
+    ? products.filter((x) => x.skus.find((y) => y.size == size))
+    : products;
+  if (error) throw error;
+  if (loading) return <Spinner></Spinner>;
   return (
     <>
       <div className="content">
@@ -76,10 +90,14 @@ if(loading) return <Spinner></Spinner>;
         <main>
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size" value={size} onChange={
-              (e) =>{
+            <select
+              id="size"
+              value={size}
+              onChange={(e) => {
                 // debugger;
-                setSize(e.target.value)}}>
+                setSize(e.target.value);
+              }}
+            >
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
@@ -87,9 +105,7 @@ if(loading) return <Spinner></Spinner>;
             </select>
             {size && <h2>Found {filteredProducts.length}</h2>}
           </section>
-          <section id="products">
-          {filteredProducts.map(renderProduct)}
-          </section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
