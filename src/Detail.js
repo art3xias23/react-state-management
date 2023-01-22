6f257094f6e38a7d5086965f07831c93ec3e3337
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-22 21:00:50 +0000

    Added Detail page

--- src/Detail.js
+++ src/Detail.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
-  return <h1>Detail</h1>;
+    const {id} = useParams();
+    console.log(`Id: ${id}`)
+    const {data: product, loading, error} = useFetch(`products/${id}`)
 
-  // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+    console.log("Data: ");
+    console.dir(product);
+
+  if (loading) return <Spinner></Spinner>;
+  if(!product) return <PageNotFound></PageNotFound>
+  if (error) throw error;
+//   TODO: Display these products details
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 }
