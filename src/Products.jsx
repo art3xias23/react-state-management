73ebff072ebd37a58208971dc9770c798cac87a9
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-22 20:27:09 +0000

    Added useParams for category pages

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
+import { useParams } from "react-router-dom";
 
 export default function Products() {
   const [size, setSize] = useState("");
+  const {category} = useParams();
 
   const {
     data: products,
     loading,
     error,
-  } = useFetch("products?category=shoes");
+  } = useFetch(`products?category=${category}`);
 
   function renderProduct(p) {
     return (
