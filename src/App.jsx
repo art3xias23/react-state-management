099d0f480e6d3624ae3c4c9b0472d3cc775be4da
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-22 20:14:19 +0000

    Added seperate products component

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import { getProducts } from "./services/productService";
-import Spinner from "./Spinner";
-import useFetch from "./services/useFetch";
-
+import Products from "./Products";
 
 export default function App() {
-  const [size, setSize] = useState("");
-  
-const {data : products, loading, error} = useFetch("products?category=shoes");
-  
-
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
-  const filteredProducts = size
-    ? products.filter((x) => x.skus.find((y) => y.size == size))
-    : products;
-  if (error) throw error;
-  if (loading) return <Spinner></Spinner>;
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select
-              id="size"
-              value={size}
-              onChange={(e) => {
-                // debugger;
-                setSize(e.target.value);
-              }}
-            >
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            {size && <h2>Found {filteredProducts.length}</h2>}
-          </section>
-          <section id="products">{filteredProducts.map(renderProduct)}</section>
+          <Products/>
         </main>
       </div>
       <Footer />
