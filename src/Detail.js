fdbf8dd6df12331c3d250fe28a72dab95bebdf60
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-23 07:28:54 +0000

    Added dynamic size selection option

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
@@ -6,6 +6,7 @@ import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
     const {id} = useParams();
+    const [size, setSize] = useState();
     console.log(`Id: ${id}`)
     const {data: product, loading, error} = useFetch(`products/${id}`)
     const navigate = useNavigate();
@@ -22,8 +23,19 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <select
+          id="size"
+          value={size}
+          onChange={(e) => {
+            // debugger;
+            setSize(e.target.value);
+          }}
+        >
+          <option value="">What size?</option>
+          {product.skus.map((x) => <option key={x.sku}>{x.size}</option>)}
+        </select>
       <p>
-        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+        <button className="btn btn-primary" onClick={() => navigate("/cart")} disabled={!size}>
             Add to Cart
         </button>
       </p>
