bc96423c22146216ef1b2a142b70d484a9afceb4
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-24 20:57:35 +0000

    Added ref to check if component is mounted and if a value changed

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,33 +1,38 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-export default function useFetch(url){
-const [data, setData] = useState(null);
+export default function useFetch(url) {
+  const isMountedRef = useRef(false);
+  const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  
+
   useEffect(() => {
+    isMountedRef.current = true;
     async function init() {
       try {
         const response = await fetch(baseUrl + url);
 
-        if(response.ok) {
-            const json = await response.json();
-        setData(json);
-        } else{
-            throw response;
+        if (response.ok) {
+          const json = await response.json();
+          if(isMountedRef.current) setData(json);
+        } else {
+          throw response;
         }
       } catch (error) {
-        setError(error);
-      }
-      finally{
-        setLoading(false);
+        if(isMountedRef.current) setError(error);
+      } finally {
+        if(isMountedRef.current)setLoading(false);
       }
     }
 
     init();
+
+    return () => {
+      isMountedRef.current = false;
+    }
   }, [url]);
 
-  return {data, error, loading};
+  return { data, error, loading };
 }
