1faa8495f6f895e83930045a229238bc11cbee9c
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-25 09:51:52 +0000

    Added context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -7,21 +7,10 @@ import { Routes, Route } from "react-router-dom";
 import Cart from "./Cart";
 import Detail from "./Detail";
 import Checkout from "./Checkout";
-import cartReducer from "./cartReducer";
 import { CartContext } from "./CartContext";
 
-let initialCart;
-  try {
-    initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
-  } catch (error) {console.error("The cart could not get parsed into json", error)
-initialCart= []}
-
 export default function App() {
-  const [cart, dispatch] = useReducer(cartReducer, initialCart);
 
-  useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart]);
 
   return (
     <CartContext.Provider value={{cart, dispatch}}>
--- src/CartContext.js
+++ src/CartContext.js
@@ -1,3 +1,23 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
+import cartReducer from "./cartReducer";
 
-export const CartContext = React.createContext(null); 
+export const CartContext = React.createContext(null);
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch (error) {
+  console.error("The cart could not get parsed into json", error);
+  initialCart = [];
+}
+
+export function CartProvider(props) {
+
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+const contextValue = {cart, dispatch}
+  return <CartContext.Provider value={contextValue}>props.children</CartContext.Provider>;
+}
